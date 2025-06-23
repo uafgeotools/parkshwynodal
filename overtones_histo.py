27ffb73779f21f52f10b5a467cf95a37efdfd12d
Isabella Seppi <irseppi@alaska.edu>
2025-06-22 17:56:52 -0800

    Plot and organize results

---
+++ overtones_histo.py
@@ -0,0 +1,66 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+flights = ['B737', 'B738', 'B739'] #, 'B77W', 'B772', 'B788', 'B789', 'B763', 'B744','B733','B732','B77L','B748','CRJ2', 'A332', 'A359', 'A359', 'E75S']
+flights = ['B190','BE20','C208','DH8A','AT73','SW4','PC12','DH3T','C441','B18T','B350','BE10']
+flights = ['AS50','R44'] 
+flights = ['PA31','DHC2','GA8','C180','C182','C206','C172','PA32','PA46','CH7B','PA30','C46','BE35','PA18','PA34'] #'C185'
+
+
+equip_overtone_dict = {}
+count_dict = {}
+color_dict = {}
+
+y = 0
+for eq in  flights:
+    # Define the directory where your files are located
+    file = 'output/Inversion_Results/'+eq+'data_atmosphere_full.txt'
+    if eq not in equip_overtone_dict:
+        equip_overtone_dict[eq] = []
+        count_dict[eq] = []
+        color_dict[eq] = np.random.rand(3,)
+    with open(file, 'r') as f:
+
+        # Read the data from the file and append it to the list
+        for line in f.readlines():
+            y += 1
+            data = []
+            counts = []
+            lines = line.split(',')
+            peaks = np.array(lines[7])
+            peaks = str(peaks)  # Replace "string" with "str"
+            peaks = np.array(peaks.split(' '))
+            for peak in peaks:
+                try:
+                    peak = float(peak)
+                except:
+                    continue
+                data.append(peak)
+                counts.append(float(y))
+            equip_overtone_dict[eq].extend(data)
+            count_dict[eq].extend(counts)
+
+plt.figure(figsize=(10, 6))
+for equip, peaks in equip_overtone_dict.items():
+    x = np.array(peaks)
+    y = np.array(count_dict[equip])
+    color = color_dict[equip]
+    plt.scatter(peaks, y, c=color)
+plt.legend(equip_overtone_dict.keys(), loc='upper right', fontsize='small')
+plt.xlim(0, 305)
+plt.xticks(np.arange(0, 305, 5))
+plt.grid(True)
+plt.show()
+
+
+plt.figure()
+for equip, peaks in equip_overtone_dict.items():
+    x = np.array(peaks)
+
+    color = color_dict[equip]
+    plt.hist(peaks, color=color, bins=61)
+plt.legend(equip_overtone_dict.keys(), loc='upper right', fontsize='small')
+plt.xticks(np.arange(0, 305, 5))
+plt.xlim(0,305)
+#plt.ylim(0.7, 7)
+plt.show()
