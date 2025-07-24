65cb0f43eff217ae86555e8d61e27aafc301f47c
Isabella Seppi <irseppi@alaska.edu>
2025-07-23 17:06:23 -0800

    Covert Jupyter Notebook to scripts

--- scripts/data_download/Downsampling/500_to_50_spect.py
+++ scripts/data_download/Downsampling/500_to_50_spect.py
@@ -1,28 +1,63 @@
-%matplotlib inline
+
 import faulthandler
 import numpy as np
 import matplotlib.pyplot as plt
 import obspy 
 
 from obspy.signal.filter import bandpass
-from prelude import *
+from tqdm import trange, tqdm
 
 faulthandler.enable()
-tr = obspy.read("/home/irseppi/nodal_data/500sps/2019_02_11/ZE_1001_DPZ.msd")
-tr_new = obspy.read("/home/irseppi/nodal_data/50sps/2019_02_11/ZE_1001_DPZ.msd")
 
-fig1, ax1 = plt.subplots()
-fig1.set_figwidth(100.0)
-fig1.set_figheight(30.0)
-ax1.set_xlim([0, 3500])
-tr.spectrogram(axes = ax1,dbscale=True)
-plt.savefig("assets/example500spec.png")
-plt.show()
+tr = obspy.read("/scratch/irseppi/nodal_data/500sps/2019_02_11/ZE_1001_DPZ.msd")
+tr_new = obspy.read("/scratch/irseppi/nodal_data/50sps/2019_02_11/ZE_1001_DPZ.msd")
 
-fig2, ax2 = plt.subplots()
-fig2.set_figwidth(100.0)
-fig2.set_figheight(30.0)
-ax2.set_xlim([0, 3500])
-tr_new.spectrogram(axes = ax2,dbscale=True)
-plt.savefig("assets/example50spec.png")
-plt.show()
+# Filtering with a bandpass on a copy of the original Trace
+tr_filt = tr.copy()
+tr_filt.filter('bandpass', freqmin=1, freqmax=25)
+
+# Filtering with a bandpass on a copy of the downsampled Trace
+tr_new_filt = tr_new.copy()
+tr_new_filt.filter('bandpass', freqmin=1, freqmax=25)
+
+for x in range(len(tr_filt)):
+
+    t = np.arange(0, tr_filt[x].stats.npts / tr_filt[x].stats.sampling_rate, tr_filt[x].stats.delta)
+
+    t_new = np.arange(0, tr_new_filt[x].stats.npts / tr_new_filt[x].stats.sampling_rate,tr_new_filt[x].stats.delta)
+    
+    plt.figure().set_figwidth(15)
+
+    plt.plot(t, tr_filt[x].data, 'b', label='Raw', alpha=0.5)
+
+    plt.plot(t_new[0:len(tr_new_filt[x])], tr_new_filt[x].data, 'r', label='Downsampled', alpha=0.4)
+
+    plt.ylabel('Bandpassed Data')
+    plt.xlabel('Time [s]')
+
+    plt.suptitle(tr[x].stats.station+'.'+tr[x].stats.channel)
+    plt.xlim(863,875)
+    plt.legend()
+
+    plt.show()
+  
+
+    t = np.arange(0, tr[x].stats.npts / tr[x].stats.sampling_rate, tr[x].stats.delta)
+
+    t_new = np.arange(0, tr_new[x].stats.npts / tr_new[x].stats.sampling_rate,tr_new[x].stats.delta)
+
+    plt.figure().set_figwidth(15)
+
+    plt.plot(t, tr[x].data, 'b', label='Raw', alpha=0.5)
+
+    plt.plot(t_new[0:len(tr_new[x])], tr_new[x].data, 'r', label='Downsampled', alpha=0.4)
+
+    plt.ylabel('Bandpassed Data')
+    plt.xlabel('Time [s]')
+
+    plt.suptitle(tr[x].stats.station+'.'+tr[x].stats.channel)
+    plt.xlim(863,875)
+    plt.legend()
+
+    plt.show()
+  
