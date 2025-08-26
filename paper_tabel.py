67ae1a5af227f64dd6ec62e327c26fbedd18565c
Isabella Seppi <irseppi@alaska.edu>
2025-08-26 14:12:28 -0800

    Update Table for paper

--- paper_tabel.py
+++ paper_tabel.py
@@ -1,38 +1,113 @@
 import pandas as pd
-import numpy as np
+import os
 
-infile = open('input/node_crossings_db_UTM.txt', 'r')
+with open('input/node_crossings_db_UTM.txt', 'r') as infile:
+	infile_lines = infile.readlines()
+Equipment = ['DH8A', 'B190','BE20','PC12','DH3T','C208','AT73','SW4','C441','B18T','B350','BE10','AS50','R44','C185','PA31','DHC2','GA8','C180','C182','C206','C172','PA32','PA46','CH7B','PA30','C46','BE35','PA18','PA34']
 
-equip_counts = {}  # Define the "equip_counts" dictionary before the loop
-flight_nums = {}  # Define the "flight_nums" dictionary before the loop
-crossings = {}
-engine_types = {}
+equip_data = pd.read_csv('input/20231010_Aircraft_UA_Fairbanks.csv', sep=",")
+man = equip_data['MANUFACTURER']
+model = equip_data['Model']
+engine = equip_data['Engine Type']
+engine_count = equip_data['Engine Count']
+wake_turb = equip_data['Wake Turbulence Category']
+equip_type = equip_data['Type Designator']
+
 engine_counts = {}
 wake_turbs = {}
+type_name = {}
+engine_type_dict = {}
+man_dict = {}
 
-for line in infile:
-	data = line.split(',')  # Split the line by commas
-	equip = data[-2]  # Get the equipment type from the line
-	if equip not in equip_counts:
-		file = open('output/inv_results_old/' + equip + '_full_inv_results.csv', 'r')
-		for ll in file:
-			l = ll.split(',')
-			flight_num = l[0]
-			sta = l[2]
-			closest_time = l[3]
-			key = (flight_num, sta, closest_time)
+flight_nums = {}  # Define the "flight_nums" dictionary before the loop
+crossings = {}
+tail_numbers = {}
+nodes_count = {}
+
+flight_nums_inverted = {}
+crossings_inverted = {}
+tail_numbers_inverted = {}
+node_inverted = {}
+
+for key in Equipment:
+	flight_nums[key] = []
+	crossings[key] = 0
+	tail_numbers[key] = []
+	nodes_count[key] = []
+	flight_nums_inverted[key] = []
+	crossings_inverted[key] = 0
+	tail_numbers_inverted[key] = []
+	node_inverted[key] = []
+	for eq_type in equip_type:
+		if str(eq_type) == str(key):
+			ind = equip_type[equip_type == eq_type].index[0]
+			engine_counts[key] = engine_count[ind]
+			wake_turbs[key] = wake_turb[ind]
+			type_name[key] = model[ind]
+			engine_type_dict[key] = engine[ind]
+			man_dict[key] = man[ind]
+			break
+		else:
+			continue
+	for line in infile_lines:
+		data = line.split(',')  # Split the line by commas
+		equip = data[10]  # Get the equipment type from the line
+		if equip not in Equipment:
+			continue
+		if equip == key:
 			crossings[key] = crossings.get(key, 0) + 1
-	if equip == np.nan or equip == 'nan':
-		equip = 'Unknown'
-	flight_num = data[1]
-	if flight_num not in flight_nums:
-		flight_nums[equip] = []
-		flight_nums[equip].extend([flight_num]) 
-	equip_counts[equip] = equip_counts.get(equip, 0) + 1  # move outside loop to get count of crossings instead of counts of flights
+			flight_num = data[1]
+			if flight_num not in flight_nums[key]:
+				flight_nums[key].extend([flight_num])
+			sta = data[9]
+			date = data[0]
+			flight_data = pd.read_csv('/scratch/irseppi/nodal_data/flightradar24/' + str(date) + '_flights.csv', sep=",")
+			flight = flight_data['flight_id']
+			tailnumber = flight_data['aircraft_id']
+			for i, f_id in enumerate(flight):
+				if str(f_id) == str(flight_num):
+					tailnum = tailnumber[i]
+					break
+				else:
+					continue
+			if tailnum not in tail_numbers[key]:
+				tail_numbers[key].extend([tailnum])
+			if sta not in nodes_count[key]:
+				nodes_count[key].extend([sta])
 
-print(equip_counts)
-print(len(equip_counts))
-print(len(flight_nums))
+	inv_file = 'output/inv_results/' + key + '_full_inv_results.txt'
+	if not os.path.exists(inv_file):
+		continue
+	file = open('output/inv_results/' + key + '_full_inv_results.txt', 'r')
+	for ll in file:
+		l = ll.split(',')
+		if str(l[-1]) == 'Forward Model':
+			continue
+		crossings_inverted[key] = crossings_inverted.get(key, 0) + 1
+		f_num = l[1]
+		if f_num not in flight_nums_inverted[key]:
+			flight_nums_inverted[key].extend([f_num])
+		sta = l[2]
+		if sta not in node_inverted[key]:
+			node_inverted[key].extend([sta])
+		date = l[0]
+		flight_data = pd.read_csv('/scratch/irseppi/nodal_data/flightradar24/' + str(date) + '_flights.csv', sep=",")
+		flight = flight_data['flight_id']
+		tailnumber = flight_data['aircraft_id']
+		for i, f_id in enumerate(flight):
+			if str(f_id) == str(f_num):
+				tailnum = tailnumber[i]
+				break
+			else:
+				continue
+		if tailnum not in tail_numbers_inverted[key]:
+			tail_numbers_inverted[key].extend([tailnum])
 
+final_table = open('paper_table.txt', 'w')
+# Write the header row
+final_table.write('Equipment,Manufacturer,Model,Engine Type,Engine Count,Wake Turbulence Category,Total Flights,Inverted Flights,Total Crossings,Inverted Crossings,Total Tail Numbers,Inverted Tail Numbers,Total Nodes,Inverted Nodes\n')
+for eq in Equipment:
 
+	final_table.write(eq +',' + str(man_dict[eq]) + ',' + str(type_name[eq]) + ',' + str(engine_type_dict[eq]) + ',' + str(engine_counts[eq]) + ',' + str(wake_turbs[eq]) + ',' + str(len(flight_nums[eq])) + ',' + str(len(flight_nums_inverted[eq])) + ',' + str(crossings[eq]) + ',' + str(crossings_inverted[eq]) + ',' + str(len(tail_numbers[eq])) + ',' + str(len(tail_numbers_inverted[eq])) + ',' + str(len(nodes_count[eq])) +  ',' + str(len(node_inverted[eq])) + '\n')
 
+final_table.close()
