b2f68c7cd8aed834db4d3d50940cd7a55e880e6c
Isabella Seppi <irseppi@alaska.edu>
2025-09-09 11:06:29 -0800

    Organize files

--- scripts/planes_flightradar/stats/TSR_Paper_STATS/paper_tabel.py
+++ scripts/planes_flightradar/stats/TSR_Paper_STATS/paper_tabel.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 jets = True
-with open('input/node_crossings_db_UTM.txt', 'r') as infile:
+with open('/home/irseppi/REPOSITORIES/parkshwynodal/input/node_crossings_db_UTM.txt', 'r') as infile:
 	infile_lines = infile.readlines()
 
 Equipment = ['DH8A', 'B190','BE20','PC12','DH3T','C208','AT73','SW4','C441','B18T','B350','BE10','AS50','R44','C185','PA31','DHC2','GA8','C180','C182','C206','C172','PA32','PA46','CH7B','PA30','C46','BE35','PA18','PA34']
