e87f9953826789ba1d032de3994446aa2e40b3c1
Isabella Seppi <irseppi@alaska.edu>
2025-10-20 14:59:18 -0800

    Remove unused files

--- scripts/soundscape/distance_highway/comb_hwy.py
+++
@@ -1,9 +0,0 @@
-#Combine 3 Parks Highway text files into 1
-import fileinput
-import glob
-
-file_list = glob.glob("*.txt")
-
-with open('result.txt', 'w') as file:
-    input_lines = fileinput.input(file_list)
-    file.writelines(input_lines)
--- scripts/soundscape/distance_highway/reformat_road.py
+++
@@ -1,8 +0,0 @@
-#after converting 3 Parks Highway kml(https://dot.alaska.gov/stwddes/gis/transdata/GE_Files/Alaska_DOTPF_RoadSystem_081114.kml) files to text files reformat to be just lat and lon
-import sys
-import fileinput
-
-for i, line in enumerate(fileinput.input('PARKSHIGHWAY3.txt', inplace=1)):
-    sys.stdout.write(line.replace(',0', '\n')) 
-    sys.stdout.write(line.replace('</coordinates>..<coordinates>', '  '))
-
