2e65e278e5ab690d66436791696f4f50e2e344cd
Isabella Seppi <irseppi@alaska.edu>
2025-10-23 13:16:55 -0800

    Update file paths

--- scripts/planes_flightradar/flight_stats/paper_tabel.py
+++ scripts/planes_flightradar/flight_stats/paper_tabel.py
@@ -1,14 +1,16 @@
 import pandas as pd
 
+repo_path = '/home/irseppi/REPOSITORIES/parkshwynodal/'
+flightradar24_data_path = '/scratch/irseppi/nodal_data/flightradar24/'
 jets = False
-with open('/home/irseppi/REPOSITORIES/parkshwynodal/input/node_crossings_db_UTM.txt', 'r') as infile:
+with open(repo_path + 'input/node_crossings_db_UTM.txt', 'r') as infile:
 	infile_lines = infile.readlines()
 if not jets:	
 	Equipment = ['DH8A', 'B190','BE20','PC12','DH3T','C208','AT73','SW4','C441','B18T','B350','BE10','AS50','R44','C185','PA31','DHC2','GA8','C180','C182','C206','C172','PA32','PA46','CH7B','PA30','C46','BE35','PA18','PA34']
 else:
 	Equipment = ['B737','B738','B739','B77W','B772','B789','B788','B733','B763','A359','B77L','B744','E75S','B732','A332','B748','nan','CRJ2']
 
-equip_data = pd.read_csv('input/20231010_Aircraft_UA_Fairbanks.csv', sep=",")
+equip_data = pd.read_csv(repo_path + 'input/20231010_Aircraft_UA_Fairbanks.csv', sep=",")
 man = equip_data['MANUFACTURER']
 model = equip_data['Model']
 engine = equip_data['Engine Type']
@@ -33,8 +35,6 @@ tail_numbers_inverted = {}
 node_inverted = {}
 
 for key in Equipment:
-	#if str(key) == 'nan':
-	#	key = 'Unknown'
 	flight_nums[key] = []
 	crossings[key] = 0
 	tail_numbers[key] = []
@@ -66,7 +66,7 @@ for key in Equipment:
 				flight_nums[key].extend([flight_num])
 			sta = data[9]
 			date = data[0]
-			flight_data = pd.read_csv('/scratch/irseppi/nodal_data/flightradar24/' + str(date) + '_flights.csv', sep=",")
+			flight_data = pd.read_csv(flightradar24_data_path + str(date) + '_flights.csv', sep=",")
 			flight = flight_data['flight_id']
 			tailnumber = flight_data['aircraft_id']
 			for i, f_id in enumerate(flight):
@@ -82,8 +82,8 @@ for key in Equipment:
 				tail_numbers[key].extend([tailnum])
 			if sta not in nodes_count[key]:
 				nodes_count[key].extend([sta])
-				
-	file = open('NGT_flight_param_inv_DB.txt', 'r')
+
+	file = open(repo_path + 'output/NGT_flight_param_inv_DB.txt', 'r')
 	for i, ll in enumerate(file):
 		if i == 0:
 			continue
@@ -91,10 +91,9 @@ for key in Equipment:
 
 		if str(l[-2]) == 'Forward Model':
 			continue
-		if str(l[0]) != str(key): # and str(l[0]) != 'nan' and key != 'Unknown':
+		if str(l[0]) != str(key):
 			continue	
-		#if key == 'Unknown':
-		#	print('uk')
+
 		crossings_inverted[key] = crossings_inverted.get(key, 0) + 1
 
 		f_num = l[2]
@@ -104,7 +103,7 @@ for key in Equipment:
 		if sta not in node_inverted[key]:
 			node_inverted[key].extend([sta])
 		date = l[1]
-		flight_data = pd.read_csv('/scratch/irseppi/nodal_data/flightradar24/' + str(date) + '_flights.csv', sep=",")
+		flight_data = pd.read_csv(flightradar24_data_path + str(date) + '_flights.csv', sep=",")
 		flight = flight_data['flight_id']
 		tailnumber = flight_data['aircraft_id']
 		for i, f_id in enumerate(flight):
@@ -118,9 +117,9 @@ for key in Equipment:
 			tail_numbers_inverted[key].extend([tailnum])
 	file.close()
 if not jets:
-	final_table = open('paper_table.txt', 'w')
+	final_table = open(repo_path + 'paper_table.txt', 'w')
 else:
-	final_table = open('paper_table_jets.txt', 'w')
+	final_table = open(repo_path + 'paper_table_jets.txt', 'w')
 
 # Write the header row
 final_table.write('Equipment,Manufacturer,Model,Engine Type,Engine Count,Wake Turbulence Category,Total Flights,Inverted Flights,Total Crossings,Inverted Crossings,Total Tail Numbers,Inverted Tail Numbers,Total Nodes,Inverted Nodes\n')
@@ -150,6 +149,7 @@ for eq in Equipment:
 	total_inverted_nodes += len(node_inverted[eq])
 final_table.write('Totals,' + ',' + ',' + ',' + ',' + ',' + str(total_flights) + ',' + str(total_inverted_flights) + ',' + str(total_crossings) + ',' + str(total_inverted_crossings) + ',' + str(total_tail_numbers) + ',' + str(total_inverted_tail_numbers) + ',' + str(total_nodes) +  ',' + str(total_inverted_nodes) + '\n')
 final_table.close()
+
 #In dataset not only inverted
 infile = open('/home/irseppi/REPOSITORIES/parkshwynodal/input/node_crossings_db_UTM.txt', 'r')
 Equipment = ['DH8A', 'B190','BE20','PC12','DH3T','C208','AT73','SW4','C441','B18T','B350','BE10','AS50','R44','C185','PA31','DHC2','GA8','C180','C182','C206','C172','PA32','PA46','CH7B','PA30','C46','BE35','PA18','PA34']
@@ -172,12 +172,14 @@ for line in infile.readlines():
 		prop_crossings += 1
 	elif equip in Equipment_jet:
 		total_jet_crossings += 1
+print('Crossings in dataset (not only inverted):')
 print('Total NaN Crossings: ' + str(total_nan_crossings))
 print('Total Prop Crossings: ' + str(prop_crossings))
 print('Total Jet Crossings: ' + str(total_jet_crossings))
 print('Total Crossings: ' + str(total_nan_crossings + prop_crossings + total_jet_crossings))
+
 #Inverted
-infile = open('/home/irseppi/REPOSITORIES/parkshwynodal/NGT_flight_param_inv_DB.txt', 'r')
+infile = open(repo_path + 'output/NGT_flight_param_inv_DB.txt', 'r')
 Equipment = ['DH8A', 'B190','BE20','PC12','DH3T','C208','AT73','SW4','C441','B18T','B350','BE10','AS50','R44','C185','PA31','DHC2','GA8','C180','C182','C206','C172','PA32','PA46','CH7B','PA30','C46','BE35','PA18','PA34']
 Equipment_jet = ['B737','B738','B739','B77W','B772','B789','B788','B733','B763','A359','B77L','B744','E75S','B732','A332','B748','CRJ2']
 total_nan_crossings = 0
@@ -198,6 +200,7 @@ for line in infile.readlines():
 		prop_crossings += 1
 	elif equip in Equipment_jet:
 		total_jet_crossings += 1
+print('Crossings in dataset that were inverted for:')
 print('Total NaN Crossings: ' + str(total_nan_crossings))
 print('Total Prop Crossings: ' + str(prop_crossings))
 print('Total Jet Crossings: ' + str(total_jet_crossings))
