383ac380ae8456da8e2c484db5ad79080991cdf7
Isabella Seppi <irseppi@alaska.edu>
2026-06-11 14:31:53 -0800

    Fix variable names

--- getting_started/forward_model.py
+++ getting_started/forward_model.py
@@ -13,73 +13,75 @@ import matplotlib.pyplot as plt
 
 # Default doppler shift parameters
 DEFAULTS = {
-    'f0': 150,
-    'tprime0': 120,
+    'v': 80,
+    'd0': 4000,
+    't0': 120,
     'c': 320,
-    'v0': 80,
-    'L': 4000,
+    'fs': 150
 }
 
 # Variables ranges for plotting differing results for changes in each variable
 VAR_RANGES = {
     'base': np.arange(0, 241, 1),
-    'v0': np.arange(0, 200, 20),
-    'L': np.arange(0, 5000, 200),
+    'v': np.arange(0, 200, 20),
+    'd0': np.arange(0, 5000, 200),
+    't0': np.arange(0, 240, 20),
     'c': np.arange(200, 500, 5),
-    'tprime0': np.arange(0, 240, 20),
-    'f0': np.arange(0, 250, 20),
+    'fs': np.arange(0, 250, 20),
 }
 
 # Time array for plotting
 time_receiver = VAR_RANGES['base']
 
 
-def get_t(v0, L, c, tprime0, time_receiver, org=False):
+def get_t(v, d0, t0, c, time_receiver, org=False):
     """Calculate time in aircraft reference frame from 
     spectrogram reference frame.
 
-    :type v0: numpy.int64 float
-    :param v0: Velocity of the aircraft
-    :type L: numpy.int64 float
-    :param L: Altitude of the aircraft
+    :type v: numpy.int64 float
+    :param v: Velocity of the aircraft
+    :type d0: numpy.int64 float
+    :param d0: Altitude of the aircraft
     :type c: numpy.int64 float
     :param c: Velocity of the wave propagation
-    :type tprime0: numpy.int64 float
-    :param tprime0: Time of closest approach in spectrogram reference frame
+    :type t0: numpy.int64 float
+    :param t0: Time of closest approach in spectrogram reference frame
     :type time_receiver: np.array float
     :param time_receiver: Time array in spectrogram reference frame
+    :type org: bool
+    :param org: If True, return original time array
     :rtype: np.array
     :return: Time array in aircraft reference frame
     """
-    beta = v0/c
+    beta = v/c
     if org:
-        arg = time_receiver - tprime0
+        arg = time_receiver - t0
     else:
-        arg = time_receiver - tprime0 + L/c
-    discriminant = arg**2 - (1 - beta**2) * (arg**2 - (L/c)**2)
+        arg = time_receiver - t0 + d0/c
+    discriminant = arg**2 - (1 - beta**2) * (arg**2 - (d0/c)**2)
     return (arg - np.sqrt(discriminant)) / (1 - beta**2)
 
 
-def get_f(v0, L, c, t_array, f0):
+def get_f(v, d0, tprime_array, c, fs):
     """Calculate observed frequency (Doppler effect).
 
-    :type v0: numpy.int64 float
-    :param v0: Velocity of the aircraft
-    :type L: numpy.int64 float
-    :param L: Altitude of the aircraft
+    :type v: numpy.int64 float
+    :param v: Velocity of the aircraft
+    :type d0: numpy.int64 float
+    :param d0: Altitude of the aircraft
     :type c: numpy.int64 float
     :param c: Velocity of the wave propagation
-    :type t_array: np.array
-    :param t_array: Time array in aircraft reference frame
-    :type f0: numpy.int64 float
-    :param f0: Emitted frequency from aircraft
+    :type tprime_array: np.array
+    :param tprime_array: Time array in aircraft reference frame
+    :type fs: numpy.int64 float
+    :param fs: Emitted frequency from aircraft
     :rtype: np.array
     :return: Frequency array of received frequency at seonsors corresponding 
-    to t_array
+    to tprime_array
     """
-    base = np.sqrt(L**2 + (v0 * t_array)**2)
+    base = np.sqrt(d0**2 + (v * tprime_array)**2)
     base_checked = np.where(base != 0, base, 1e-10)
-    return f0 / (1 + (v0/c) * (v0*t_array) / base_checked)
+    return fs / (1 + (v/c) * (v*tprime_array) / base_checked)
 
 
 # Create subplots
@@ -93,12 +95,13 @@ for n, var_name in enumerate(var_names):
     params = DEFAULTS.copy()
 
     # Base plot with default parameters
-    tprime = get_t(params['v0'], params['L'], params['c'], 
-                params['tprime0'], time_receiver)
-    ft = get_f(params['v0'], params['L'], params['c'], tprime, params['f0'])
+    tprime_array = get_t(params['v'], params['d0'], params['t0'], params['c'], 
+                   time_receiver)
+    print(tprime_array)
+    ft = get_f(params['v'], params['d0'], tprime_array, params['c'], params['fs'])
 
     axs[n].plot(time_receiver, ft, c='k', linewidth=0.5, zorder=10)
-    axs[n].axvline(params['tprime0'], c='k', linewidth=0.5, zorder=10)
+    axs[n].axvline(params['t0'], c='k', linewidth=0.5, zorder=10)
     axs[n].set_title(f'Varying {var_name}')
     axs[n].set_ylim(100, 225)
     axs[n].set_xlim(0, 240)
@@ -120,10 +123,10 @@ for n, var_name in enumerate(var_names):
         # Plot for varying parameters 
         for val in var_values:
             params[var_name] = val
-            tprime = get_t(params['v0'], params['L'], params['c'], 
-                        params['tprime0'], time_receiver)
-            ft = get_f(params['v0'], params['L'], params['c'], tprime, 
-                    params['f0'])
+            tprime_array = get_t(params['v'], params['d0'], params['t0'], params['c'], 
+                           time_receiver)
+            ft = get_f(params['v'], params['d0'], tprime_array, params['c'], 
+                    params['fs'])
             axs[n].plot(time_receiver, ft, color=cm(norm(val)), linewidth=0.5)
 plt.tight_layout()
 plt.show()
